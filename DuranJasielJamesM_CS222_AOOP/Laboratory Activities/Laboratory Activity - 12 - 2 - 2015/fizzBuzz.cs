177552087512a3ec0fd2a8de68ab782b9c34c96f
vandal1er <165736672+vandal1er@users.noreply.github.com>
2025-02-12 16:45:11 +0800

    Update fizzBuzz.cs

--- DuranJasielJamesM_CS222_AOOP/Laboratory Activities/Laboratory Activity - 12 - 2 - 2015/fizzBuzz.cs
+++ DuranJasielJamesM_CS222_AOOP/Laboratory Activities/Laboratory Activity - 12 - 2 - 2015/fizzBuzz.cs
@@ -1,6 +1,26 @@
 using System;
 
-namespace FizzBuzz
+public class FizzBuzz
 {
-
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            if ((i+1) % 3 == 0)
+            {
+                Console.Write("Fizz");
+                
+            }
+            if ((i+1) % 5 == 0)
+            {
+                Console.Write("Buzz");
+                
+            }
+            if ((i+1) % 3 != 0 && (i+1) % 5 != 0)
+            {
+                Console.Write(i+1);
+            }
+            Console.WriteLine();
+        }
+    }
 }
