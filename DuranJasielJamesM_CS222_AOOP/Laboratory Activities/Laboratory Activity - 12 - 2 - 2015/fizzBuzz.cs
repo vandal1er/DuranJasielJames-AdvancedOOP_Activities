5be07202f0d6bc3a1db417d789fe035e09567b9d
vandal1er <165736672+vandal1er@users.noreply.github.com>
2025-02-12 17:12:26 +0800

    Update fizzBuzz.cs

--- DuranJasielJamesM_CS222_AOOP/Laboratory Activities/Laboratory Activity - 12 - 2 - 2015/fizzBuzz.cs
+++ DuranJasielJamesM_CS222_AOOP/Laboratory Activities/Laboratory Activity - 12 - 2 - 2015/fizzBuzz.cs
@@ -4,21 +4,26 @@ public class FizzBuzz
 {
     public static void Main(string[] args)
     {
-        for (int i = 0; i < 15; i++)
+        int upBound;
+        
+        Console.Write("Enter upper bound: ");
+        upBound = int.Parse(Console.ReadLine());
+        
+        for (int i = 1; i <= upBound; i++)
         {
-            if ((i+1) % 3 == 0)
+            if ((i) % 3 == 0)
             {
                 Console.Write("Fizz");
                 
             }
-            if ((i+1) % 5 == 0)
+            if ((i) % 5 == 0)
             {
                 Console.Write("Buzz");
                 
             }
-            if ((i+1) % 3 != 0 && (i+1) % 5 != 0)
+            if ((i) % 3 != 0 && (i) % 5 != 0)
             {
-                Console.Write(i+1);
+                Console.Write(i);
             }
             Console.WriteLine();
         }
